57187e9f81218e101c0693943ef4ff9668ec2b65
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-09-18 16:48:34 +0200

    fix: make GetTowerType not unnecessarily async

--- Game.Persistence/Repositories/TowerRepository.cs
+++ Game.Persistence/Repositories/TowerRepository.cs
@@ -11,6 +11,6 @@ public class TowerRepository(TowerDefenseContext dbContext)
 
     public FinalTower? GetFinalTower(int pathId) => DbContext.FinalTowers.FirstOrDefault(x => x.PathId == pathId);
 
-    public async Task<ComputedTower?> GetTowerType(Guid towerId) =>
-        await DbContext.ComputedTowers.FindAsync(towerId);
+    public ComputedTower? GetTowerType(Guid towerId) =>
+        DbContext.ComputedTowers.FirstOrDefault(t => t.Id == towerId);
 }
